971807b3bf1e539732cf10dae076767e2c54c0ef
Pim Snel <post@pimsnel.com>
2022-06-28 19:14:14 +0200

    new step in workflow

---
+++ .github/workflows/getmeta.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+readarray -t arr < <(jq '.links' templates.json)
+
+for i in "${arr[@]}"
+do
+  if  [ "$i" = "[" ] || [ "$i" = "]" ]
+  then
+    echo ""
+  else
+    link=$(echo "$i" | tr -d '"' | tr -d ',')
+    echo $link/quiqr/etalage/template.json
+  fi
+
+done
+
